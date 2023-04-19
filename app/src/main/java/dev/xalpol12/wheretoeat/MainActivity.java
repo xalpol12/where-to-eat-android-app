1375dfcd6ba6bf97e16f81970bb7b217f75ec1a9
Dawid Stawiszynski <dawid.stawiszynski@student.put.poznan.pl>
2023-04-19 23:56:31 +0200

    init commit

---
+++ app/src/main/java/dev/xalpol12/wheretoeat/MainActivity.java
@@ -0,0 +1,20 @@
+package dev.xalpol12.wheretoeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // screen diagonal:     5 inches
+    // screen dimensions:   W 2.45 : H 4.36
+    // aspect ratio:        16:9 (~294 ppi density)
+    // pixel resolution:    720x1080
+    // dp resolution:       391x587
+    // density bucket:      xhdpi
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
