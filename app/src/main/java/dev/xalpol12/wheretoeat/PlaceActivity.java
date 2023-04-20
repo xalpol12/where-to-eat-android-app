8d4b18dc040e9bfad747a1a5687e4519b2137d7d
Dawid Stawiszynski <dawid.stawiszynski@student.put.poznan.pl>
2023-04-21 01:43:46 +0200

    Add place activity

---
+++ app/src/main/java/dev/xalpol12/wheretoeat/PlaceActivity.java
@@ -0,0 +1,14 @@
+package dev.xalpol12.wheretoeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place);
+    }
+}
---
+++ app/src/main/java/dev/xalpol12/wheretoeat/PlaceDetails.java
@@ -0,0 +1,14 @@
+package dev.xalpol12.wheretoeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlaceDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_details);
+    }
+}
