155b616c2c05802901b6dbd46ac080364bb091f8
Dawid Stawiszynski <dawid.stawiszynski@student.put.poznan.pl>
2023-05-02 18:41:28 +0200

    Add annotations

---
+++ app/src/main/java/dev/xalpol12/wheretoeat/network/dto/PlaceRequestDTO.java
@@ -0,0 +1,21 @@
+package dev.xalpol12.wheretoeat.network.dto;
+
+import dev.xalpol12.wheretoeat.data.utility.Location;
+import dev.xalpol12.wheretoeat.data.utility.PlaceType;
+import dev.xalpol12.wheretoeat.data.utility.PriceLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaceRequestDTO {
+    Location location;
+    int distance;
+    PriceLevel minPrice;
+    PriceLevel maxPrice;
+    PlaceType type;
+}
