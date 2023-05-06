708e0601a63a054a3bc0415abc3ae4af4b99846a
Dawid Stawiszynski <dawid.stawiszynski@student.put.poznan.pl>
2023-05-07 01:13:11 +0200

    add annotation for serializing enum

--- app/src/main/java/dev/xalpol12/wheretoeat/data/utility/PriceLevel.java
+++ app/src/main/java/dev/xalpol12/wheretoeat/data/utility/PriceLevel.java
@@ -1,13 +1,20 @@
 package dev.xalpol12.wheretoeat.data.utility;
 
-
 import androidx.annotation.NonNull;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum PriceLevel {
+
+    @SerializedName("0")
     FREE("0"),
+    @SerializedName("1")
     INEXPENSIVE("1"),
+    @SerializedName("2")
     MODERATE("2"),
+    @SerializedName("3")
     EXPENSIVE("3"),
+    @SerializedName("4")
     VERY_EXPENSIVE("4");
 
     private final String priceLevel;
@@ -16,6 +23,7 @@ public enum PriceLevel {
         this.priceLevel = priceLevel;
     }
 
+    @NonNull
     public String toString() {
         return this.priceLevel;
     }
