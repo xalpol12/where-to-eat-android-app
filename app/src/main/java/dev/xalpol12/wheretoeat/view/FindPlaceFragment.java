0c6dc0d07ba46561e3e00748c293a60ab40387c2
Dawid Stawiszynski <dawid.stawiszynski@student.put.poznan.pl>
2023-05-23 00:57:38 +0200

    add fragments for main menu and hamburger menu options

---
+++ app/src/main/java/dev/xalpol12/wheretoeat/view/FindPlaceFragment.java
@@ -0,0 +1,20 @@
+package dev.xalpol12.wheretoeat.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import dev.xalpol12.wheretoeat.R;
+
+public class FindPlaceFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_find_place, container, false);
+    }
+}
